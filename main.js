745143d7ed77401ea3e295561343cd25234ef445
mitskixw <143209910+mitskixw@users.noreply.github.com>
2024-09-23 10:37:53 -0300

    9

--- main.js
+++ main.js
@@ -13,3 +13,10 @@ let areaTriangulo = function(base,altura){
     return(base*altura)/2;
 }
 console.log(areaTriangulo(6,7))
+
+let nome = prompt('quanto é 2+2?');
+let nome = prompt('quanto é 2+8?');
+let nome = prompt('quanto é 0+2?');
+document.write(4);
+document.write(10);
+document.write(2);
