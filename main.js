161d00cf4d54e215bd005f7afa587e0a1c09c505
mitskixw <143209910+mitskixw@users.noreply.github.com>
2024-09-30 09:33:49 -0300

    .

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-alert('Emanuelli Emidio Almeida e Gabielle Gaby')
-function soma(num1, num2) {
-    return num1 + num2;
-}
-soma(2, 3);
-console.log(soma(2, 3));
-function areaQuadrado(lado) {
-    return lado ** 2
-}
-console.log(areaQuadrado(6));
+ // alert('Emanuelli Emidio Almeida e Gabielle Gaby')
+// function soma(num1, num2) {
+//     return num1 + num2;
+// }
+// soma(2, 3);
+// console.log(soma(2, 3));
+// function areaQuadrado(lado) {
+//     return lado ** 2
+// }
+// console.log(areaQuadrado(6));
 
-let areaTriangulo = function (base, altura) {
-    return (base * altura) / 2;
-}
-console.log(areaTriangulo(6, 7))
+// let areaTriangulo = function (base, altura) {
+//     return (base * altura) / 2;
+// }
+// console.log(areaTriangulo(6, 7))
 
-let num1 = parseInt(prompt('numero'));
-let num2 = parseInt(prompt('numero'));
-let num3 = parseInt(prompt('numero'));
+// let num1 = parseInt(prompt('numero'));
+// let num2 = parseInt(prompt('numero'));
+// let num3 = parseInt(prompt('numero'));
 
-function media(x, y, z) {
+// function media(x, y, z) {
 
-    return (x + y + z) / 3
-}
+//     return (x + y + z) / 3
+// }
 
-document.write(media(num1, num2, num3));
+// document.write(media(num1, num2, num3));
 
 let estados = ['Parana', 'Mato Grosso', 'Para', 'Santa Cantarina', 'Rio de Janeiro', 'Bahia', 'Sao Paulo', 'Mato Grosso do Sul', 'Distrito Federal', 'Goias', 'Alagoas', 'Ceara', 'Maranhao', 'Pernambuco', 'Piaui', 'Paraiba', 'Rio Grande do Norte', 'Sergipe', 'Acre', 'Amapa', 'Amazonas', 'Rondonia', 'Roraima', 'Tocantins', 'Espiito Santos', 'Minas Gerais'];
